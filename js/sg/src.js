76fc49505a3862de4612d837fd652745d9a93c1f
RareSpark <lokiownsyou@gmail.com>
2017-05-05 07:28:24 -0400

    Update src.js

--- js/sg/src.js
+++ js/sg/src.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 
 	$("#btn-rawtext, .menu-link").hover(function() {
-		$(this).css("background-color", "#000038");
+		$(this).css("background-color", "#1b1b79");
 	},function() {
 		$(this).css("background-color", "#000038");
 	});
 
 	$("#fldLeft, #fldMiddleT, #fldRight, #fldMiddleB").hover(function() {
-		$(this).css("background-color", "#000038");
+		$(this).css("background-color", "#F0027E");
 	}, function() {
 		$(this).css("background-color", "#000038");
 	});
